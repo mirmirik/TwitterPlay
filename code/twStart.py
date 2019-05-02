43e287ddcfc699ad32758d51e2e86a20387b603b
Tolga MIRMIRIK <tolga@mirmirik.net>
2019-05-02 20:58:07 +0300

    Dizin işaretçileri düzenlendi

--- code/twStart.py
+++ code/twStart.py
@@ -17,10 +17,10 @@ def hitTwitter():
     TW_ACCOUNT = cfg.get('auth', 'ACCOUNT')
     CONSUMER_KEY = cfg.get('auth', 'CONSUMER_KEY')
     CONSUMER_SECRET = cfg.get('auth', 'CONSUMER_SECRET')
-    MY_TWITTER_CREDS = os.path.expanduser('../config/.tw_credentials_' + TW_ACCOUNT)
+    MY_TWITTER_CREDS = os.path.expanduser('..//config//.tw_credentials_' + TW_ACCOUNT)
 
-    if not os.path.exists("data"):
-        os.makedirs("data")
+    if not os.path.exists("..//data"):
+        os.makedirs("..//data")
 
     if not os.path.exists(MY_TWITTER_CREDS):
         oauth_dance("TW_App", 
