7cb25338cd28c4852fe674d781c87f82711ad366
Tolga MIRMIRIK <tolga@mirmirik.net>
2019-05-15 14:58:28 +0300

    Fixed AB#39

--- code/removeFollowed.py
+++ code/removeFollowed.py
@@ -26,7 +26,7 @@ from datetime import datetime, date
 TODAY_FORMATTED = datetime.today().strftime('%Y%m%d')
 
 rules = { 'LAST_STATUS_UPDATE': 6, 
-            'ACCOUNT_CREATED_YEAR': 2010, #date.today().year,
+            'ACCOUNT_CREATED_YEAR': 2008, #date.today().year,
             'ACCOUNT_CREATED_MONTH': date.today().month }
 
 def removeFriend():
@@ -34,7 +34,7 @@ def removeFriend():
 
     tw = twStart.hitTwitter()
 
-    index = 1
+    removeFriendCount = 1
 
     while activeCursor != 0:
 
@@ -44,29 +44,27 @@ def removeFriend():
         removeableUsers = {0:{}}
 
         for usr in f["users"]:
-            # print (usr["status"])
             try: 
                 if (usr["status"]):
-                    lastInteraction = twStart.FormatTwitterDate(usr["status"]["created_at"])
-                    accountCreated = twStart.FormatTwitterDate(usr["created_at"])
+                    lastInteraction = twStart.FormatTwitterDate(usr["status"]["created_at"], 'D')
+                    accountCreated = twStart.FormatTwitterDate(usr["created_at"], 'D')
                     # En son 2019'dan önce tweet atmışları takipten çıkaralım.
-                    if (int(accountCreated[:4]) < rules['ACCOUNT_CREATED_YEAR']):
-                        removeableUsers = {index: {}}
-                        removeableUsers[index]["user_id"] = usr["id_str"]
-                        removeableUsers[index]["user_name"] = usr["name"]
-                        removeableUsers[index]["account_created"] = accountCreated
-                        removeableUsers[index]["last_interaction"] = lastInteraction
-                        removeableUsers[index]["remove_reason"] = "Last interaction date is too old."
-                        print(removeableUsers)
-                        index += 1
+                    if (accountCreated.year < rules['ACCOUNT_CREATED_YEAR']):
+                        lastInteraction = twStart.FormatTwitterDate(usr["status"]["created_at"], 'S')
+                        accountCreated = twStart.FormatTwitterDate(usr["created_at"], 'S')
+                        removeableUsers = {removeFriendCount: {}}
+                        removeableUsers[removeFriendCount]["user_id"] = usr["id_str"]
+                        removeableUsers[removeFriendCount]["user_name"] = usr["name"]
+                        removeableUsers[removeFriendCount]["account_created"] = accountCreated
+                        removeableUsers[removeFriendCount]["last_interaction"] = lastInteraction
+                        removeableUsers[removeFriendCount]["remove_reason"] = "Last interaction date is too old."
+                        removeFriendCount += 1
                     #     tw.friendships.destroy(user_id=usr["id_str"])
             except: # status alınamıyorsa, kullanıcı "kilitli" hesaba sahiptir.
                 lastInteraction = "Not found"
 
-            
-
             # print("{:4d} {:25s} {:25s} {:25s} {:25s}".format(
-            #     index,
+            #     removeFriendCount,
             #     usr["screen_name"], 
             #     usr["name"], 
             #     lastInteraction, 
@@ -75,5 +73,19 @@ def removeFriend():
 
         activeCursor = f["next_cursor"]
 
+
+    print(removeableUsers)
+    
+    for p_id, p_info in removeableUsers.items():
+        print("\nremoveFriendCount:", p_id)
+        for key in p_info:
+            print(key + ':', p_info[key])
+
+
+
 if __name__ == "__main__": 
+    # t = "Sat Aug 19 12:51:00 +0000 2019"
+    # print (twStart.FormatTwitterDate(t, 'S'))
+    # print(twStart.FormatTwitterDate(t,'D'))
+    # print(twStart.FormatTwitterDate(t))
     removeFriend()
