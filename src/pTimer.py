8dc9274ce422ba815fda7b43616ed9e1c07cc38a
Tolga MIRMIRIK <tolga@mirmirik.net>
2019-09-19 10:18:27 +0300

    Update pTimer.py

--- src/pTimer.py
+++ src/pTimer.py
@@ -1,11 +1,13 @@
 from time import time
 from functools import wraps
 import random
+import pytest
 
 def timeit(func):
     """
     :param func: Decorated function
     :return: Execution time for the decorated function
+    TM GIT Test
     """
 
     @wraps(func)
@@ -25,3 +27,19 @@ def sort_rnd_num():
     numbers = [random.randint(100, 200) for _ in range(100000)]
     numbers.sort()
     return numbers
+
+
+def PrintNumber(a, b):
+    result = a + b
+
+    if (result >= 0):
+        return "Positive"
+    else:
+        return "Negative"
+
+@pytest.mark.parametrize('testDataA, testDataB, shouldBe', 
+        [(3, 5, "Positive"), 
+         (-2, -5, "Negative")])
+def test_TwitterDateAsString(testData, shouldBe):
+    result = PrintNumber(testDataA, testDataB)
+    assert result == shouldBe, "PrintNumber is not working correctly."
